9b3a01e2f958833bf87f0ec0e0c5fc8ebfe77dbe
[John Michira] <[johnbrownmichira@hotmail.com]>
2022-09-09 13:38:09 +1000

    Added controller mappings to controller class

--- src/main/java/com/s3437237/itemmanagement/controller/ItemController.java
+++ src/main/java/com/s3437237/itemmanagement/controller/ItemController.java
@@ -3,10 +3,9 @@ package com.s3437237.itemmanagement.controller;
 import com.s3437237.itemmanagement.model.Item;
 import com.s3437237.itemmanagement.model.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ItemController {
@@ -16,12 +15,37 @@ public class ItemController {
 
     @RequestMapping("")
     public String viewHomePage() {
-        return "Welcome to Item Management";
+        return "Welcome to Item Management - project by John Brown [ s3437237 ]";
     }
 
+    // create a new item
     @PostMapping("/item")
     public String addItem(@RequestBody Item item) {
         return itemService.addItem(item);
+    }
+
+    // Get specific item by id
+    @GetMapping("/item/item/{id}")
+    public Item getItem(@PathVariable int id) {
+        return itemService.getItem(id);
+    }
+
+    // Get all items
+    @RequestMapping(value = "/item/item", method = RequestMethod.GET)
+    public List<Item> getAllItems(){
+        return itemService.getAllItems();
+    }
+
+    // Update item
+    @RequestMapping(value = "/item/item", method = RequestMethod.PUT)
+    public String updateItem(@RequestBody Item item) {
+        return itemService.updateItem(item);
+    }
+
+    // Delete item
+    @RequestMapping(value = "/item/item", method = RequestMethod.DELETE)
+    public String deleteItem(@RequestBody Item item) {
+        return itemService.deleteItem(item.getId());
     }
 
 }
